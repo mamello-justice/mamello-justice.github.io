b4adc89ea18012b153423d0050b5074fff40751e
J. Seboholi <j.seboholi@gmail.com>
2020-05-22 12:10:06 +0200

    Finished home page

---
+++ precache-manifest.be05d61089694c99a622f4c6be271acb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b845155481fb64792565694117778096",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47154d52391206d7b604",
+    "url": "/static/css/main.7e67f443.chunk.css"
+  },
+  {
+    "revision": "6506b9af1ec3740fa25a",
+    "url": "/static/js/2.a8d1a780.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/static/js/2.a8d1a780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47154d52391206d7b604",
+    "url": "/static/js/main.8ce7dc0c.chunk.js"
+  },
+  {
+    "revision": "37f61398e45abe37a8b1",
+    "url": "/static/js/runtime-main.09488799.js"
+  },
+  {
+    "revision": "41b0794ddc28bc833f7e2ec06ce85df2",
+    "url": "/static/media/IMG_2230.41b0794d.JPG"
+  }
+]);
