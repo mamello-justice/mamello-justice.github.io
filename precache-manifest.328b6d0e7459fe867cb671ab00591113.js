073887e5b5738d049fb2cb42b778baab88321b8b
J. Seboholi <j.seboholi@gmail.com>
2020-06-12 17:59:43 +0200

    Update: Change profile image and created section headings

---
+++ precache-manifest.328b6d0e7459fe867cb671ab00591113.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd53b8abeeda2cefac459ce0bad4daa0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49f62b79716107397ab3",
+    "url": "/static/css/main.1925d009.chunk.css"
+  },
+  {
+    "revision": "4a8863d3c988413630b6",
+    "url": "/static/js/2.7882e961.chunk.js"
+  },
+  {
+    "revision": "890b83c3b503af9a5630afd9ff112890",
+    "url": "/static/js/2.7882e961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49f62b79716107397ab3",
+    "url": "/static/js/main.49188090.chunk.js"
+  },
+  {
+    "revision": "37f61398e45abe37a8b1",
+    "url": "/static/js/runtime-main.09488799.js"
+  },
+  {
+    "revision": "194429deb4e7b0d432c970f7fd7ca338",
+    "url": "/static/media/IMG_2230.194429de.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b3c9d24edd2be73f16583a140c057ca3.js"
+  "/precache-manifest.328b6d0e7459fe867cb671ab00591113.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
